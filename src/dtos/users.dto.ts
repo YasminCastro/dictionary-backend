7a188057e199e2610db36d850df1b9936f5da9a6
yasmin <yasminsdcastro@gmail.com>
2022-09-18 13:42:34 -0300

    refactor: validations

--- src/dtos/users.dto.ts
+++ src/dtos/users.dto.ts
@@ -1,13 +1,15 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   public email: string;
 
   @IsString()
+  @MinLength(5)
   public password: string;
 
   @IsString()
+  @MinLength(3)
   public name: string;
 }
 
